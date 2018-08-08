ffb1665450ca29b975b3c02746fb8dc67af40c92
xichao <95877589@qq.com>
2018-08-08 14:18:06 +0800

    view:mata/form/datetime recover datetimePicker

--- views/assets/javascripts/app/mobile.js
+++ views/assets/javascripts/app/mobile.js
@@ -2,4 +2,9 @@ $(function () {
   $('.qor-mobile--show-actions').on('click', function () {
     $('.qor-page__header').toggleClass('actions-show');
   });
+
+  if($(".js-custom_datepicker").get(0)) {
+    $(".js-custom_datepicker").datetimepicker({ format: 'Y-m-d H:i', timepicker: true});
+    $.datetimepicker.setLocale('ch');
+  }
 });
